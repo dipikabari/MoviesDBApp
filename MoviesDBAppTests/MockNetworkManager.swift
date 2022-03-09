b158583de5a69b46d3d7b3707d19092ead9124dc
Dipika Bari <dipikab@y7mail.com>
2022-03-09 15:31:54 +0000

    Adding unit tests

---
+++ MoviesDBAppTests/MockNetworkManager.swift
@@ -0,0 +1,29 @@
+//
+//  MockNetworkManager.swift
+//  MoviesDBAppTests
+//
+//  Created by Dipika Bari on 09/03/2022.
+//
+
+import Foundation
+@testable import MoviesDBApp
+import Combine
+
+class MockNetworkManager: Networkable {
+    func doApiCall(apiRequest: ApiRequestType) -> Future<Data, ServiceError> {
+        return Future { promise in
+            
+            let bundle = Bundle(for:MockNetworkManager.self)
+            
+            guard let url = bundle.url(forResource:apiRequest.path, withExtension:"json"),
+                  let data = try? Data(contentsOf: url)
+
+            else {
+                promise(.failure(ServiceError.dataNotFound))
+          
+                return
+            }
+            promise(.success(data))
+        }
+    }
+}
