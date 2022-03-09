7292ff0f8a0abf16225b1277920f961cee21547f
Dipika Bari <dipikab@y7mail.com>
2022-03-09 15:08:59 +0000

    Adding Utility folder

---
+++ MoviesDBApp/Utility/Extensions/SearchBar+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar+Extension.swift
+//  Movies
+//
+//  Created by Dipika Bari on 09/03/2022.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField } // receiving notifications with objects which are instances of UITextFields
+            .compactMap(\.text) // extracting text and removing optional values (even though the text cannot be nil)
+            .eraseToAnyPublisher()
+    }
+}
